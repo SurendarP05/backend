d97f018bf6e0eedb24fa1b0f23549a289d911205
SurendarP05 <100403580+SurendarP05@users.noreply.github.com>
2024-03-10 18:31:04 +0530

    check

--- login/serializer.py
+++ login/serializer.py
@@ -12,12 +12,15 @@ class LoginSerializer(serializers.ModelSerializer):
         fields=['email','password']
 
     def validate(self, data):
+        print(data)
         email = data.get("email")
         password = data.get("password")
         try:
             user = User.objects.get(email=email)
+            print(user)
             if user.check_password(password):
                 data['user'] = user
+                print(data)
         except User.DoesNotExist:
             raise serializers.ValidationError('User Not Found')
         return data
